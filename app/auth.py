47c43512e82e84fcb99b7fa9974bbc832c75d56c
PARMAR PRANAM <161856663+coolguyp@users.noreply.github.com>
2025-12-17 15:50:54 +0530

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -1,1 +1,15 @@
+import jwt
+from datetime import datetime, timedelta
+
+SECRET_KEY = "supersecretkey"
+
+def generate_token(user_id):
+    payload = {
+        "user_id": str(user_id),
+        "exp": datetime.utcnow() + timedelta(hours=2)
+    }
+    return jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+
+def verify_token(token):
+    return jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
 
